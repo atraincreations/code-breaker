483d099c27b2a09da3933721e859fa16650d4dd7
Alex Underwood <underwoodapu@gmail.com>
2020-09-16 22:23:11 -0700

    99 percent complete
    the app is fully functioning. nearly every bug has been checked for and resolved.

--- app.js
+++ app.js
@@ -1,27 +1,53 @@
 // Hint Variables
 var remainingHintCount = 3;
-var currentProblem = "One";
+var currentProblem = "One"; // Default first problem
 var hintText;
 var hintOneCount = -1; // Count of how many times hint used per problem for hint tracking
-var hintTwoCount = -1;
+var hintTwoCount = -1; // -1 for easy incrimenting to first array index
 var hintThreeCount = -1;
 
 // Part One Color Variables
-var colorOne; // Should be:
-var colorTwo; // Should be:
-var colorThree; // Should be:
-var colorFour; // Should be:
+var colorOne; // Should be: Yellow
+var colorTwo; // Should be: Green
+var colorThree; // Should be: Blue
+var colorFour; // Should be: Red
 
 // Part Two Direction Variables
-var directionOne; // Should be:
-var directionTwo; // Should be:
-var directionThree; // Should be:
-var directionFour; // Should be:
+var directionOne; // Should be: South
+var directionTwo; // Should be: East
+var directionThree; // Should be: North
+var directionFour; // Should be: West
 
-// Hide additional problems until previous is solved
+// Part Three Direction Number Variables
+var numberOne; // Should be: 22
+var numberTwo; // Should be: 14
+var numberThree; // Should be: 1
+var numberFour; // Should be: 17
+
+// Hide additional problems and initialize numbers
 window.onload = function(){
     document.getElementById('part-two-container').style.display = 'none';
+    document.getElementById('part-three-container').style.display = 'none';
+    //document.getElementById('you-win').style.display = 'none';
     $("#hints-remaining").text(remainingHintCount);
+}
+
+// Hint Count Function
+// Check count and increase and re-enable if required
+function hitCountCheck() {
+    if (remainingHintCount < 3 ) {
+        remainingHintCount++;
+        // update text in container
+        $("#hints-remaining").text(remainingHintCount);
+        $('.hint-text').text("");
+        // If the count is now 1 that means we need to turn section back on
+        if ( remainingHintCount == 1 ) {
+            $('#hint-btn').prop('disabled', false); // re-enable button
+            // update text in container
+            $("#hints-remaining").text(remainingHintCount);
+            $('.hint-text').text("");
+        }
+    }
 }
 
 // Hint tracking and insertion dependent upon current problem
@@ -40,15 +66,28 @@ $("#hint-btn").on("click",function(){
             hintOneCount++;
             console.log(hintOneCount);
             hintText = [
-                "Hint 1",
-                "Hint 2",
-                "Hint 3"
+                "Don't watch too close, it will hurt your eyes..",
+                "I'm not just for ambience..",
+                "It all starts with yellow."
             ];
             $('.hint-text').text(hintText[hintOneCount]);
             break;
         case "Two":
-
+            hintTwoCount++;
+            hintText = [
+                "You can't drink your way out..",
+                "Things always need to be put in order..",
+                "Go check the kitchen.."
+            ];
+            $('.hint-text').text(hintText[hintTwoCount]);
             break;
+        case "Three":
+            hintThreeCount++;
+            hintText = [
+                "Children are the future..",
+                "Never ask a kid for directions..",
+                "Maybe kids really do know it all.."
+            ];
         default:
             break;
     }
@@ -57,7 +96,7 @@ $("#hint-btn").on("click",function(){
 })
 
 
-
+// Y/G/B/R
 // Part One color order check
 $("#check-color-btn").on("click",function(){
     // Grab value of each dropdown picker
@@ -65,10 +104,64 @@ $("#check-color-btn").on("click",function(){
     colorTwo = $( "#color-2 option:selected" ).text();
     colorThree = $( "#color-3 option:selected" ).text();
     colorFour = $( "#color-4 option:selected" ).text();
-    console.log("Test:", colorOne);
 
     // Check for solution
+    if (colorOne == "yellow" && colorTwo == "green" && colorThree == "blue" && 
+        colorFour == "red") {
+            $("#error").text("Complete!")
+            // Upon completion update problem number and make next problem visible
+            document.getElementById('part-two-container').style.display = 'block';
+            currentProblem = "Two";
+            hitCountCheck();
+    } else {
+        console.log("try again")
+        $("#error").text("try again")
+    }
 
-    // Upon completion update problem number and make next problem visible
+})
+
+// S/E/N/W
+// Check direction order from bottles
+$("#check-direction-btn").on("click",function(){
+    // Grab input from each box
+    directionOne = $( "#direction-1 option:selected" ).text();
+    directionTwo = $( "#direction-2 option:selected" ).text();
+    directionThree = $( "#direction-3 option:selected" ).text();
+    directionFour = $( "#direction-4 option:selected" ).text();
+
+    // Check for solution
+    if (directionOne == "south" && directionTwo == "east" && directionThree == "north" && 
+        directionFour == "west") {
+            $("#error-two").text("Complete!")
+            // Upon completion update problem number and make next problem visible
+            document.getElementById('part-three-container').style.display = 'block';
+            currentProblem = "Three";
+            hitCountCheck();
+    } else {
+        console.log("try again");
+        $("#error-two").text("try again");
+    }
+
+})
+
+// 22/14/1/17
+// Check direction number
+$("#check-number-btn").on("click",function(){
+    // Grab input from each box
+    numberOne = $("#number-1").val();
+    numberTwo = $("#number-2").val();
+    numberThree = $("#number-3").val();
+    numberFour = $("#number-4").val();
+
+    // Check for solution
+    if (numberOne == "22" && numberTwo == "14" && numberThree == "1" && numberFour == "17") {
+            // GAME WON
+            $("#error-three").text("Complete!")
+            document.getElementById('you-win').style.display = 'block';
+            $("#win-hints-remaining").text("You had", remainingHintCount, "left");
+    } else {
+        console.log("try again");
+        $("#error-three").text("try again");
+    }
 
 })
